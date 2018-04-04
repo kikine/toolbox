6c9408ef3b5299d5fc6dd348bb12c8ea74ef4ffd
shuaicj <shuaicj@gmail.com>
2018-04-04 11:12:31 +0800

    more ec2 scripts

--- aws/ec2-create.sh
+++ aws/ec2-create.sh
@@ -3,6 +3,7 @@
 function create()
 {
     name=
+    group=
     ec2=
     ebs=
     ip=
@@ -11,6 +12,7 @@ function create()
     do
         case "$1" in
             --name)  name="$2"; shift 2;;
+            --group) group="$2"; shift 2;;
             --ec2)   ec2="$2"; shift 2;;
             --ebs)   ebs="$2"; shift 2;;
             --ip)    ip="$2"; shift 2;;
@@ -24,8 +26,8 @@ function create()
         --subnet-id subnet-ffffffff \
         --private-ip-address "$ip" \
         --block-device-mappings "DeviceName=/dev/sda1,Ebs={VolumeType=gp2,VolumeSize=$ebs}" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$name},{Key=Owner,Value=shuaicj}]" \
-                             "ResourceType=volume,Tags=[{Key=Name,Value=$name},{Key=Owner,Value=shuaicj}]" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$name},{Key=Group,Value=$group},{Key=Owner,Value=shuaicj}]" \
+                             "ResourceType=volume,Tags=[{Key=Name,Value=$name},{Key=Group,Value=$group},{Key=Owner,Value=shuaicj}]" \
         --security-group-ids sg-ffffffff \
         --key-name your-key-name
 }
@@ -34,6 +36,7 @@ function batchCreate()
 {
     namePrefix=
     nameFrom=
+    group=
     ec2=
     ebs=
     ipPrefix=
@@ -45,6 +48,7 @@ function batchCreate()
         case "$1" in
             --name-prefix)  namePrefix="$2"; shift 2;;
             --name-from)    nameFrom="$2"; shift 2;;
+            --group)        group="$2"; shift 2;;
             --ec2)          ec2="$2"; shift 2;;
             --ebs)          ebs="$2"; shift 2;;
             --ip-prefix)    ipPrefix="$2"; shift 2;;
@@ -58,15 +62,16 @@ function batchCreate()
     do
         name=`printf "%s-%03d" $namePrefix $[$nameFrom + $i]`
         ip="$ipPrefix.$[$ipFrom + $i]"
-        create --name $name --ec2 $ec2 --ebs $ebs --ip $ip
+        create --name $name --group $group --ec2 $ec2 --ebs $ebs --ip $ip
     done
 }
 
-create --name shuaicj-test-single --ec2 t2.micro --ebs 50 --ip 10.10.10.10
+create --name shuaicj-test-single --group shuaicj-test --ec2 t2.micro --ebs 50 --ip 10.10.10.10
 
 batchCreate \
     --name-prefix shuaicj-test-batch \
     --name-from 1 \
+    --group shuaicj-test \
     --ec2 t2.micro \
     --ebs 50 \
     --ip-prefix 10.10.10 \
---
+++ aws/ec2-stop.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import sys
+import boto3
+
+GROUP_PREFIX = 'shuaicj-'
+
+GROUP_NAMES = [
+    'group1',
+    'group2',
+]
+
+def usage():
+    print('USAGE: %s %s' % (sys.argv[0], '|'.join(GROUP_NAMES)))
+
+if len(sys.argv) < 2:
+    usage()
+    exit()
+
+#
+# read the list of group
+#
+
+groupList = []
+
+for g in sys.argv[1:]:
+    if g not in GROUP_NAMES:
+        usage()
+        exit()
+    groupList.append(GROUP_PREFIX + g)
+
+
+#
+# pull ec2 instance info
+#
+
+ec2 = boto3.resource('ec2')
+
+instances = ec2.instances.filter(
+    Filters=[
+        {
+            'Name': 'tag:Name',
+            'Values': [
+                GROUP_PREFIX + '*',
+            ]
+        },
+        {
+            'Name': 'tag:Group',
+            'Values': groupList
+        },
+        {
+            'Name': 'instance-state-name',
+            'Values': [
+                'running',
+            ]
+        },
+    ]
+)
+
+
+#
+# start ec2 instances
+#
+
+def nameOf(instance):
+    for tag in instance.tags:
+        if tag['Key'] == 'Name':
+            return tag['Value']
+
+instances = sorted(instances, key=lambda instance: nameOf(instance))
+
+for instance in instances:
+    print('stop %s' % nameOf(instance))
+    instance.stop()
+
+print('Done!')
+
