e183c33b6ad169eab862d28d0d5a5e90ab29d56b
shuaicj <shuaicj@gmail.com>
2019-12-06 18:21:26 +0800

    add reproducible-dir-md5.sh

---
+++ reproducible-build/reproducible-dir-md5.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Calculate the md5 for the whole dir content recursively.
+# The result is reproducible both on MacOS and Linux.
+
+set -e
+
+if [[ $# -eq 0 ]]; then
+    echo "Usage: ./reproducible-dir-md5.sh [DIR]"
+    exit 1
+fi
+
+dir="$1"
+
+if [[ ! -d "$dir" ]]; then
+    echo "dir not exists! $dir"
+    exit 1
+fi
+
+cd "$dir"
+
+os="$(uname -s)"
+
+if [[ "$os" = "Darwin" ]]; then
+    find . -type f -exec md5 {} \; | LC_ALL=C sort -k 2 | md5
+elif [[ "$os" = "Linux" ]]; then
+    find . -type f -exec md5sum --tag {} \; | LC_ALL=C sort -k 2 | md5sum | awk '{ print $1 }'
+else
+    echo "Unsupported OS!"
+    exit 1
+fi
+
